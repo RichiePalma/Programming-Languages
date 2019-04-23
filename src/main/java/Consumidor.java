7cb1ddb06fd8eac87f9d391728c35678357817d5
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2019-04-23 17:44:26 -0500

    Indenting

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -1,33 +1,33 @@
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Consumidor extends Thread{
-	
-	int id;
-	Almacen scheme;
-	
-	public Consumidor(int id, Almacen scheme){
-		this.id = id;
-		this.scheme = scheme;
-	}
-	
-	@Override
-	public void run(){
-		while(true){
-	        Random r = new Random(System.currentTimeMillis());
-	            try {
-	                Thread.sleep(100 * r.nextInt(10));
-	            } catch (InterruptedException ex) {
-	                Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
-	            }
-	            
-	           try{
-					this.scheme.consumir(this.id);
-			  }catch(ArithmeticException e){
-					System.out.println("Consumido por  id #" + id + ": " + "'DivisionBy0 ");
-				}  
-       }
-	}
+public class Consumidor extends Thread {
+
+    int id;
+    int sleepTime;
+    Almacen scheme;
+
+    public Consumidor(int id, int sleepTime, Almacen scheme) {
+        this.id = id;
+        this.sleepTime = sleepTime;
+        this.scheme = scheme;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(this.sleepTime);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            try {
+                this.scheme.consumir(this.id);
+            } catch (ArithmeticException e) {
+                System.out.println("Consumido por  id #" + id + ": " + "'DivisionBy0 ");
+            }
+        }
+    }
 
 }
